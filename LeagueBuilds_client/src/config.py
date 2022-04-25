eceb6d8c35f96df5842e75c3417fefb9ee4cf217
CookieDecide <julian.lauermann@freenet.de>
2022-04-25 17:00:24 +0200

    enable live updates of settings

--- LeagueBuilds_client/src/config.py
+++ LeagueBuilds_client/src/config.py
@@ -12,28 +12,38 @@ position_flash = config_parser.getint('import', 'flash')
 
 gui_darkmode = config_parser.getboolean('gui', 'darkmode')
 
-def set_import_runes(import_runes):
-    config_parser.set('import', 'runes', str(import_runes))
+def set_import_runes(import_):
+    global import_runes
+    import_runes = import_
+    config_parser.set('import', 'runes', str(import_))
     with open('config.ini', 'w') as f:
         config_parser.write(f)
 
-def set_import_items(import_items):
-    config_parser.set('import', 'items', str(import_items))
+def set_import_items(import_):
+    global import_items
+    import_items = import_
+    config_parser.set('import', 'items', str(import_))
     with open('config.ini', 'w') as f:
         config_parser.write(f)
 
-def set_import_summs(import_summs):
-    config_parser.set('import', 'summs', str(import_summs))
+def set_import_summs(import_):
+    global import_summs
+    import_summs = import_
+    config_parser.set('import', 'summs', str(import_))
     with open('config.ini', 'w') as f:
         config_parser.write(f)
 
-def set_import_flash(import_flash):
-    config_parser.set('import', 'flash', str(import_flash))
+def set_import_flash(position_):
+    global position_flash
+    position_flash = position_
+    config_parser.set('import', 'flash', str(position_))
     with open('config.ini', 'w') as f:
         config_parser.write(f)
 
 
 def set_gui_darkmode(darkmode):
+    global gui_darkmode
+    gui_darkmode = darkmode
     config_parser.set('gui', 'darkmode', str(darkmode))
     with open('config.ini', 'w') as f:
         config_parser.write(f)
