e14ae98311a47350f1533d2951bbf6f1a4f01836
CookieDecide <julian.lauermann@freenet.de>
2023-04-14 16:20:41 +0200

    api endpoint for one and 3 runesets added

--- LeagueBuilds_server/src/server.py
+++ LeagueBuilds_server/src/server.py
@@ -6,6 +6,7 @@ from models.statics_db import CHAMPIONS
 import version
 from models.log_db import CONNECTION, PLAYER
 import logging, os
+import ast
 
 if (not os.path.exists('../../../log')):
     os.mkdir('../../../log')
@@ -68,6 +69,62 @@ class Builds(Resource):
 
         buffer = {}
         buffer["championId"] =  build.championId
+        rune = ast.literal_eval(build.runes)
+        if isinstance(rune, list):
+            rune = rune[0]
+        buffer["runes"] =  str(rune)
+        buffer["summ"] =  build.summ
+        buffer["item"] =  build.item
+        buffer["start_item"] =  build.start_item
+        buffer["item_build"] =  build.item_build
+        buffer["skill_order"] =  build.skill_order
+        buffer["position"] =  build.position
+        buffer["boots"] =  build.boots
+        buffer["champion"] =  CHAMPIONS.get(CHAMPIONS.key == build.championId).champion
+
+        CONNECTION.insert(
+            time = datetime.now(),
+            ip = ip,
+            port = port,
+            championId = buffer["championId"],
+            champion = buffer["champion"],
+            position = buffer["position"]
+        ).execute()
+
+        return buffer
+    
+class Builds_V1(Resource):
+    def get(self, champion, position=''):
+        ip = str(request.remote_addr)
+        port = str(12345)
+
+        logger.info(f'{ip}\t{port}\t{request.headers.get("Summoner")}\t{champion}\t{position}')
+
+        PLAYER.insert(
+            time = datetime.now(),
+            ip = ip,
+            port = port,
+            summonername = request.headers.get("Summoner")
+        ).execute()
+
+        if(position!=''):
+            build = FINALBUILDS.get_or_none(
+                FINALBUILDS.championId == champion,
+                FINALBUILDS.position == str(position).lower()
+            )
+            if(not build):
+                build = FINALBUILDS.get_or_none(
+                    FINALBUILDS.championId == champion,
+                    FINALBUILDS.position == ""
+                )
+        else:
+            build = FINALBUILDS.get_or_none(
+                FINALBUILDS.championId == champion,
+                FINALBUILDS.position == ""
+            )
+
+        buffer = {}
+        buffer["championId"] =  build.championId
         buffer["runes"] =  build.runes
         buffer["summ"] =  build.summ
         buffer["item"] =  build.item
@@ -94,4 +151,5 @@ class Version(Resource):
         return version.version
 
 api.add_resource(Builds, '/builds/<champion>/<position>', '/builds/<champion>')
+api.add_resource(Builds_V1, '/builds_v1/<champion>/<position>', '/builds_v1/<champion>')
 api.add_resource(Version, '/version')
